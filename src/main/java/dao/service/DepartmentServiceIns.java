985054bbd05fee710f84b48c62d8b3df572c78e8
csw <11911915@mail.sustech.edu.cn>
2021-05-24 01:13:24 +0800

    finish the DepartmentServiceIns and the three remove method in the CourseServiceIns

--- src/main/java/dao/service/DepartmentServiceIns.java
+++ src/main/java/dao/service/DepartmentServiceIns.java
@@ -5,8 +5,13 @@ import cn.edu.sustech.cs307.service.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+//TODO：目前这个class的抛出异常工作都没有完成
+//TODO:这个class return的值或许需要修改（return null）
 
 public class DepartmentServiceIns implements DepartmentService{
     @Override
@@ -19,6 +24,8 @@ public class DepartmentServiceIns implements DepartmentService{
             preparedStatement.setString(1, name);
             preparedStatement.execute();
 
+
+
             connection.close();
             preparedStatement.close();
         } catch (SQLException e) {
@@ -26,12 +33,15 @@ public class DepartmentServiceIns implements DepartmentService{
         }
 
         return 0;
+        //TODO:没说return的是啥,默认为自增id,这里需要进行处理
     }
 
     @Override
     public void removeDepartment(int departmentId) {
 
-        String sql = "delete from CourseSectionClass where CourseSectionClass.id = ?";
+        //TODO:这个departmentid 的值在建表中是一个自增序列，但是实际上可能是每个department有着相对应的departmentid（也就是说这不是一个自增序列，同时建表语句需要进行修改）
+
+        String sql = "delete from department where department.id = ?";
         try {
             Connection connection = SQLDataSource.getInstance().getSQLConnection();
 
@@ -48,10 +58,31 @@ public class DepartmentServiceIns implements DepartmentService{
 
     @Override
     public List<Department> getAllDepartments() {
-        //TODO：这个方法是将所有院系的名称(String 类型)放到一个list里面
+        String sql = "select * from department where departmentId = ?;";
+        //这个方法是将所有院系的名称(String 类型)放到一个list里面
+        List<Department> departmentList = new ArrayList<>();
         //先把所有的数据选出来，然后再一条一条放到list里面去
+        ResultSet resultSet ;
+        try {
+            Connection connection = SQLDataSource.getInstance().getSQLConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+             while(resultSet.next()){
+                 Department departmentListElement = new Department();
+                 departmentListElement.id = resultSet.getInt(1);
+                 departmentListElement.name = resultSet.getString(2);
+                    departmentList.add(departmentListElement);
+             }
+
+            connection.close();
+            preparedStatement.close();
+
+            return departmentList;
 
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
--- src/main/java/dao/service/MajorServiceIns.java
+++ src/main/java/dao/service/MajorServiceIns.java
@@ -1,13 +1,34 @@
 package dao.service;
+import cn.edu.sustech.cs307.database.SQLDataSource;
 import cn.edu.sustech.cs307.dto.Major;
 import cn.edu.sustech.cs307.service.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class MajorServiceIns implements MajorService{
     @Override
     public int addMajor(String name, int departmentId) {
+        //TODO:这个return返回的是什么没有说啊，现在csw默认返回的是自增majorid
+        //TODO:这里还没有写完
+        String sql = "insert into department values (default,?)";
+        try {
+            Connection connection = SQLDataSource.getInstance().getSQLConnection();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.execute();
+
+            connection.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return 0;
+
     }
 
     @Override
